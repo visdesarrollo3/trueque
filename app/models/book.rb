d7686b084955c1568d7968926a72b61fe3e774da
Nicolás Hock Isaza <nhocki@gmail.com>
2010-12-09 22:00:18 -0500

    Added ds store to gitignore

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,10 +14,13 @@ class Book < ActiveRecord::Base
   
   belongs_to :user
   belongs_to :abstract_isbn, :class_name => "Isbn", :foreign_key => "isbn", :primary_key => "isbn"
+  
   has_many :authorships, :dependent => :destroy
   has_many :authors, :through => :authorships
   has_many :offered_trades, :class_name => "Trade", :foreign_key => "book1_id"
   has_many :receive_trades, :class_name => "Trade", :foreign_key => "book2_id"
+  
+
     
   def author_names
     @author_names || authors.map(&:name).join(', ')
