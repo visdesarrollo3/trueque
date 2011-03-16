0207e7ed9b90f02b394c5f467993c577442c9b55
Nicolás Hock Isaza <nhocki@gmail.com>
2011-03-16 07:30:08 -0500

    Books#index view

--- db/schema.rb
+++ db/schema.rb
@@ -172,7 +172,7 @@ ActiveRecord::Schema.define(:version => 20110315231954) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "featured"
+    t.boolean  "featured",    :default => false
   end
 
 end
