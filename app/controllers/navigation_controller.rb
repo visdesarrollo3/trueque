444fc66a78f78c310d19540a669549ff036a876f
Nicolás Hock Isaza <nhocki@gmail.com>
2011-04-11 17:09:13 -0500

    Always use admin books for best books

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -2,7 +2,7 @@ class NavigationController < ApplicationController
   skip_authorization_check
   def site_index
     @latest_books   = Book.newest_first.limit(8)
-    @best_books     = Book.limit(4)
+    @best_books     = User.admins.first.books.newest_first.limit(4)
     @banners        = Banner.all
     @current_survey = Survey.first
   end
