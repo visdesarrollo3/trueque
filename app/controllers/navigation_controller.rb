123b6dd2a4e9dc6ad5dbcbd2f028e455a6648c1a
Nicolás Hock Isaza <nhocki@gmail.com>
2011-06-20 11:41:53 -0500

    If main user is not found, use the recent books

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -2,7 +2,8 @@ class NavigationController < ApplicationController
   skip_authorization_check
   def site_index
     @latest_books   = Book.newest_first.available.limit(8)
-    @best_books     = User.esquina_trueque.first.books.available.newest_first.limit(4)
+    @best_books     = User.esquina_trueque.first.books.available.newest_first.limit(4) if User.esquina_trueque.first
+    @best_books   ||= Book.newest_first.available.limit(8)
     @banners        = Banner.all
     @current_survey = Survey.first
   end
