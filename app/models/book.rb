867dfc7e35419c381376d8f9434e96a7614cae83
Nicolás Hock Isaza <nhocki@gmail.com>
2011-08-02 17:19:50 -0500

    Use MySQL syntax

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Book < ActiveRecord::Base
-  default_scope order('"books"."created_at" DESC')
+  default_scope order('books.created_at DESC')
   include Pacecar
   
   attr_accessible :title, :available, :synopsis, :published_date, :editorial, :isbn, :user_id, :health_status, :health_description,
